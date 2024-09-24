e3a64903718858163c1caa48df53428fedfa39a7
spring8904 <lamthang890123music@gmail.com>
2024-09-24 09:04:01 +0700

    feat: add CORS package and enable CORS in app.js

--- src/app.js
+++ src/app.js
@@ -2,8 +2,11 @@ import express from 'express'
 import morgan from 'morgan'
 import connectMongoDB from './config/db'
 import router from './routes'
+import cors from 'cors'
 
 const app = express()
+
+app.use(cors())
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
