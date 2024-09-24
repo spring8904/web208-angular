2af710a47070444b4eb1d47187a627c0e7ed2c2e
spring8904 <lamthang890123music@gmail.com>
2024-09-24 11:12:56 +0700

    refactor: Update authValidator to make role and confirmPassword optional

--- src/validations/auth.js
+++ src/validations/auth.js
@@ -2,7 +2,7 @@ import Joi from 'joi'
 
 export const authValidator = Joi.object({
   email: Joi.string().email().required(),
-  role: Joi.string(),
+  role: Joi.string().optional(),
   password: Joi.string().min(6).required(),
-  confirmPassword: Joi.ref('password'),
+  confirmPassword: Joi.string().valid(Joi.ref('password')).required(),
 }).options({ abortEarly: false })
